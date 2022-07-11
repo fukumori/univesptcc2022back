9de2f6080497abbdc7819f511e55b436e07e404f
Levi Fukumori <levi@fukumori.com.br>
2022-07-10 22:55:42 -0300

    jetstream react

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+  .ts('resources/js/app.tsx', 'public/js')
+  .react()
+  .postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ])
+  .alias({
+    '@': 'resources/js',
+  });
+
+if (mix.inProduction()) {
+  mix.version();
+}
